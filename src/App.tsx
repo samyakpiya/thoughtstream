34bf46045c2da494df400fcffafa10446739dbe6
Samyak Piya <samyakpiya@gmail.com>
2024-03-02 20:09:08 -0500

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ButtonGradient />
+    </>
+  );
+}
+
+export default App;
